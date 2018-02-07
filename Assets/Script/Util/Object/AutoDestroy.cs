4f033135d5d34dca42ce02c8725133aafde94d03
Luyao Tian <tianluyao795@gmail.com>
2018-02-07 16:24:36 -0700

    Added tracing feature

---
+++ Assets/Script/Util/Object/AutoDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+
+    private float time;
+    public float lifetime;
+
+	// Use this for initialization
+	void Start () {
+        lifetime = 5.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (time >= lifetime)
+            Destroy(gameObject);
+        else
+            time += Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Script/Util/Scene/TraceGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceGenerator : MonoBehaviour {
+
+    public GameObject prefab;
+    private float period;
+
+	// Use this for initialization
+	void Start () {
+        period = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (period >= 0.1f)
+            period = 0.0f;
+        if (period == 0.0f)
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        period += Time.fixedDeltaTime;
+    }
+}
